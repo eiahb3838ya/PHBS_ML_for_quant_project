71e28a5f21666df0e71d8e68356d6efb7c47d377
AlfredChenZH <alfredchen0228@163.com>
2020-04-13 23:03:44 +0800

    vt+lr

--- 05 rolling prediction/RollingSignalGenerator.py
+++ 05 rolling prediction/RollingSignalGenerator.py
@@ -162,7 +162,7 @@ if __name__ =='__main__':
     
     recordModels = True
 
-    selector = SVCL1Selection
+    selector = varianceThresholdSelection
     # myPredictModel = MyDeepLearningClassifier
     myPredictModel =  MyLogisticRegClassifier
     
